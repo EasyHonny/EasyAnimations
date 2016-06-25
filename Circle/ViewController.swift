28df283351be67e24f13eecacf23b3a6d29839a7
张涛 <2583109301@qq.com>
2016-06-25 20:16:24 +0800

    Initial Commit

---
+++ Circle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Circle
+//
+//  Created by EasyHoony on 16/6/25.
+//  Copyright © 2016年 EasyHoony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
